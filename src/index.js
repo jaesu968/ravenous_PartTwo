7b9b8e716165fa8f57817ceb738ea53a657b5405
jaesu124 <mazterk968@gmail.com>
2025-04-09 14:10:08 -0500

    Part Two Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App/App";
-
+// This is Part Two of the project. 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
